41e453a83d25f3194bcff9bd570686bf4ee3a0f4
wangzy <402759136@qq.com>
2023-04-25 13:30:09 +0800

    tetst

--- src/views/LifeView.vue
+++ src/views/LifeView.vue
@@ -1,7 +1,7 @@
 <template>
     <Layout :style="{ 'height': '500px', 'background-position': 'center -120px' }" :bg="'life'">
         <template #content>
-            <Lazyload @loadSuccess="t" :src="'https://img2.baidu.com/it/u=2516443844,358800779&fm=253&fmt=auto&app=120&f=JPEG?w=667&h=500'" />
+            <Lazyload @loadSuccess="t" :src="Img" />
         </template>
     </Layout>
 </template>
@@ -9,6 +9,7 @@
 <script setup>
 import Layout from './Layout.vue';
 import Lazyload from '../components/Lazyload.vue';
+import Img from '@/assets/right.jpg';
 
 const t = (v) => console.log(v)
 </script>
