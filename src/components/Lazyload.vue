9e541e123dacf8159b8c9225f9dc542352380fcd
wangzy <402759136@qq.com>
2024-04-25 20:38:17 +0800

    lazyload

--- src/components/Lazyload.vue
+++ src/components/Lazyload.vue
@@ -1,11 +1,10 @@
 <template>
-    <img :id="props.id" style="margin-top: 20px;" :width="props.width" :height="props.height" :src="trueSrc" alt="">
+    <img :id="props.id" style="margin-top: 20px;" :width="props.width" :height="props.height" src="../assets/loaddata.gif" alt="">
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue'
 const props = defineProps(['src', 'width', 'height','id'])
-const trueSrc = ref('/src/assets/loaddata.gif')
 
 </script>
 
